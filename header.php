0238e9d6a852f65166ff179ed786c4a4ee7005e7
JAValentino <johnanthonyvalentino@gmail.com>
2019-02-10 08:50:25 -0800

    sample dashboard

---
+++ header.php
@@ -0,0 +1,37 @@
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="#" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>A</b>LT</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>Admin</b>LTE</span>
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+            <!-- User Account: style can be found in dropdown.less -->
+          <li class="dropdown user user-menu" style="margin-right: 20px;">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <img src="assets/dashboard/dist/img/avatar5.png" class="user-image" alt="User Image">
+              <span class="hidden-xs">Default</span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- Menu Body-->
+              <li class="user-body">
+                  <a href="#" class="btn btn-danger btn-flat">Sign out</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
---
+++ sidebar.php
@@ -0,0 +1,33 @@
+ <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="assets/dashboard/dist/img/avatar5.png" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Default</p>
+        </div>
+      </div><br>
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">         
+        <li>
+          <a href="#">
+            <i class="fa fa-th"></i> <span>Happy</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fa fa-book"></i> <span>New Year</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i class="fa fa-file"></i> <span>Reports</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
